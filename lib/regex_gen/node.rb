000520aa85c5b346a7ea96c551da988795c5b373
xescuGC <xescugil@gmail.com>
2015-04-09 23:43:58 +0200

    Changed the implementation of the RegexGen::Node with the respective tests

--- lib/regex_gen/node.rb
+++ lib/regex_gen/node.rb
@@ -1,17 +1,42 @@
 module RegexGen
   class Node
-    attr_accessor :childrens
+    attr_reader :parent_node
+    attr_reader :right_children
+    attr_reader :left_children
 
-    def initialize
-      @childrens = []
+    def initialize(parent_node=nil, left_children=nil, right_children=nil)
+      self.parent_node = parent_node
+      self.left_children = left_children
+      self.right_children = right_children
+    end
+
+    def right_children=(children)
+      if children.is_a?(RegexGen::Node) or children.nil?
+        @right_children = children
+      else
+        raise 'Invalid right_children type, must be a RegexGen::Node'
+      end
+    end
+
+    def left_children=(children)
+      if children.is_a?(RegexGen::Node) or children.nil?
+        @left_children = children
+      else
+        raise 'Invalid left_children type, must be a RegexGen::Node'
+      end
+    end
+
+    def parent_node=(parent)
+      if parent.is_a?(RegexGen::Node) or parent.nil?
+        @parent_node = parent
+      else
+        raise 'Invalid parent_node type, must be a RegexGen::Node'
+      end
     end
 
     def leaf?
-      @childrens.empty?
+      @left_children.nil? && @right_children.nil?
     end
 
-    def << child
-      @childrens << child
-    end
   end
 end
--- spec/unit/node_spec.rb
+++ spec/unit/node_spec.rb
@@ -2,23 +2,63 @@ describe RegexGen::Node do
   subject { RegexGen::Node }
   before(:each) do
     @node = subject.new
+    @aux_node = subject.new
   end
-  it 'must initialize it with default' do
-    expect(@node.childrens).to eq ([])
-  end
-  context '#<<' do
-    it 'must add a child to the node' do
-      @node << 'test'
-      expect(@node.childrens.count).to be(1)
-      expect(@node.childrens.first).to eq('test')
+  context 'must initialize' do
+    it 'with default values' do
+      expect(@node.right_children).to eq(nil)
+      expect(@node.left_children).to eq(nil)
+      expect(@node.parent_node).to eq(nil)
+    end
+    it 'with values' do
+      node = subject.new(@aux_node, @aux_node, @aux_node)
+      expect(node.right_children).to eq(@aux_node)
+      expect(node.left_children).to eq(@aux_node)
+      expect(node.parent_node).to eq(@aux_node)
+    end
+    it 'with errors if not valid nodes' do
+      expect{subject.new('test')}.to raise_error('Invalid parent_node type, must be a RegexGen::Node')
+      expect{subject.new(nil, 'test')}.to raise_error('Invalid left_children type, must be a RegexGen::Node')
+      expect{subject.new(nil, nil, 'test')}.to raise_error('Invalid right_children type, must be a RegexGen::Node')
     end
   end
+
+  context '#right_children=' do
+    it 'must set the value' do
+      @node.right_children = @aux_node
+      expect(@node.right_children).to eq(@aux_node)
+    end
+    it 'must throw error if it\'s not a RegexGen::Node' do
+      expect{@node.right_children = 'invalid'}.to raise_error('Invalid right_children type, must be a RegexGen::Node')
+    end
+  end
+
+  context '#left_children=' do
+    it 'must set the value' do
+      @node.left_children = @aux_node
+      expect(@node.left_children).to eq(@aux_node)
+    end
+    it 'must throw error if it\'s not a RegexGen::Node' do
+      expect{@node.left_children = 'invalid'}.to raise_error('Invalid left_children type, must be a RegexGen::Node')
+    end
+  end
+
+  context '#parent_node=' do
+    it 'must set the value' do
+      @node.parent_node = @aux_node
+      expect(@node.parent_node).to eq(@aux_node)
+    end
+    it 'must throw error if it\'s not a RegexGen::Node' do
+      expect{@node.parent_node = 'invalid'}.to raise_error('Invalid parent_node type, must be a RegexGen::Node')
+    end
+  end
+
   context '#leaf?' do
     it 'must be false with some childrens' do
       expect(@node.leaf?).to be_truthy
     end
     it 'must be true with no childrens' do
-      @node << 'child'
+      @node.left_children = @aux_node
       expect(@node.leaf?).to be_falsey
     end
   end
