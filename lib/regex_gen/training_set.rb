bb45864169351f61e032766a1c3b027ae2cbb61b
xescuGC <xescugil@gmail.com>
2015-04-12 12:36:28 +0200

    Added validations for the RegexGen::TrainingSet to be able to create it directly with examples

--- lib/regex_gen/training_set.rb
+++ lib/regex_gen/training_set.rb
@@ -2,16 +2,28 @@ module RegexGen
   class TrainingSet
     attr_accessor :examples
 
-    def initialize
+    def initialize(examples=nil)
       @examples = []
+      add_examples(examples) if examples
+      self
     end
 
     def <<(example)
-      @examples << Example.new(example)
+      add_examples(example)
     end
 
     def flush
       @examples = []
+    end
+
+    private
+    
+    def add_examples(examples)
+      if examples.is_a?(Array)
+        examples.each { |e| add_examples(e) }
+      else
+        @examples << Example.new(examples)
+      end
     end
   end
 end
--- spec/unit/training_set_spec.rb
+++ spec/unit/training_set_spec.rb
@@ -1,16 +1,33 @@
 describe RegexGen::TrainingSet do
+  subject { RegexGen::TrainingSet }
+  before(:each) do
+    @training_set = subject.new
+  end
+  describe '#new' do
+    it 'must add a new example if defined' do
+      ts = subject.new('{s:{test}}')
+      ts_array = subject.new(['{s:{test}}'])
+      expect(ts.examples.count).to eq(1)
+      expect(ts_array.examples.count).to eq(1)
+      expect(ts.examples.first.class).to eq(RegexGen::Example)
+      expect(ts_array.examples.first.class).to eq(RegexGen::Example)
+    end
+    it 'must not add nil value' do
+      expect(@training_set.examples.count).to eq(0)
+    end
+  end
   describe '#<<' do
     it 'must add a new example' do
-      subject << '{s:{test}}'
-      expect(subject.examples.count).to eq(1)
-      expect(subject.examples.first.class).to eq(RegexGen::Example)
+      @training_set << '{s:{test}}'
+      expect(@training_set.examples.count).to eq(1)
+      expect(@training_set.examples.first.class).to eq(RegexGen::Example)
     end
   end
   describe '#flush' do
     it 'must flush all the examples' do
-      subject << '{s:{test}}'
-      subject.flush
-      expect(subject.examples).to eq([])
+      @training_set << '{s:{test}}'
+      @training_set.flush
+      expect(@training_set.examples).to eq([])
     end
   end
 end
